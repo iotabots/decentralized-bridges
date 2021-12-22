10fb5197213a3eed9b69853edc50a0d26e949bc2
huhn511 <huhn511@users.noreply.github.com>
2021-12-22 13:46:26 +0000

    deploy: c59d10a7b6d68834a1c85f4193d5556c39098471

---
+++ precache-manifest.5aee8a4bcb86f6200af257ab1e010d1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a13d617e15236569893fd441b8426f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c56db738898fe8af4f64",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "c56db738898fe8af4f64",
+    "url": "/static/js/2.24730411.chunk.js"
+  },
+  {
+    "revision": "c770e37857009fae8ed9c78c1a505872",
+    "url": "/static/js/2.24730411.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e47bd6b3050cfe33e159",
+    "url": "/static/js/main.4f594e6c.chunk.js"
+  },
+  {
+    "revision": "700d31d1c6f37b7bdc51",
+    "url": "/static/js/runtime-main.84578d5e.js"
+  }
+]);
